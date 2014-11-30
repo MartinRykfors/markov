308bab389c0f5a6ece08ffe8f2707c4b4e6a29e7
Martin Rykfors <martinrykfors@gmail.com>
2014-11-30 20:32:37 +0000

    add function for adding new random char to generated

--- src/markov/generation.clj
+++ src/markov/generation.clj
@@ -1,4 +1,7 @@
-(ns markov.generation)
+(ns markov.generation
+  (:require [clojure.string :as string]))
+
+(defrecord Generation-String [current-string last-n-gram])
 
 (defn cumulative-freq-map [letter-freqs]
   (reductions (fn [[_ csum] [key freq]] [key (+ csum freq)]) letter-freqs))
@@ -7,4 +10,11 @@
 
 (defn get-weighted-char [c-freq-map]
   (let [roll (rand-int (freq-sum c-freq-map))]
-    (first (filter #(> (last %) roll) c-freq-map))))
+    (first (first (filter #(> (last %) roll) c-freq-map)))))
+
+(defn append-generated-char [gen-string freq-maps]
+  (let [c-freq-map (cumulative-freq-map (get freq-maps (:last-n-gram gen-string)))]
+    (let [new-char (get-weighted-char c-freq-map)]
+      (let [appended-string (str (:current-string gen-string) new-char)
+            new-last-n-gram (str (string/join (rest (:last-n-gram gen-string))) new-char)]
+        (Generation-String. appended-string new-last-n-gram)))))
--- test/markov/generation_test.clj
+++ test/markov/generation_test.clj
@@ -1,7 +1,12 @@
 (ns markov.generation-test
   (:require [clojure.test :refer :all]
-            [markov.generation :refer :all]))
+            [markov.generation :refer :all])
+  (:import [markov.generation Generation-String]))
+
+(def simple-map {"a" {"b" 1}})
+(def simple-seed (Generation-String. "initial " "a"))
 
 (deftest generation-test
-  (testing "test description"
-    (is (= true true))))
+  (testing "simple generation"
+    (is (= "initial b" (:current-string (append-generated-char simple-seed simple-map))))
+    (is (= "b" (:last-n-gram (append-generated-char simple-seed simple-map))))))
